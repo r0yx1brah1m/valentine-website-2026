7a22ff3ab68166395219b30214467873226d10dc
r0yx1brah1m <155172527+r0yx1brah1m@users.noreply.github.com>
2026-06-16 00:42:17 +0300

    Update valentineName to 'zoya'

--- config.js
+++ config.js
@@ -5,7 +5,8 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "pilagia",
+    valentineName: "zoya
+        ",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
